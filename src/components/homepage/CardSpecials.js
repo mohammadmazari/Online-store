3fcce54afcd37835188cbe33f853587dd954ddbf
mohammadmazari <mzarym39@gmail.com>
2025-04-12 01:48:59 +0330

    fixd bog2

--- src/components/homepage/CardSpecials.js
+++ src/components/homepage/CardSpecials.js
@@ -7,8 +7,8 @@ import { FormatNumberInPersian } from "@helper/FormatNumberInPersian";
 
 function CardSpecials({ data }) {
   return (
-    <div className=" mt-10 ">
-      <div className="mt-5 text-red-500 text-[8px] md:text-xs flex justify-between border-b-3 border-red-700 pb-1">
+    <div className=" mt-7 ">
+      <div className="mt-5 text-red-500 text-[10px] md:text-xs flex justify-between border-b-3 border-red-700 pb-1">
         <p>دیری شاپ</p>
         <p className="text-black">
           <Countdown
@@ -23,15 +23,15 @@ function CardSpecials({ data }) {
           <Image src={data.image} width={220} height={220} alt="special img" />
         </div>
         <div className="mt-1 md:mt-13">
-          <p className="text-[8px] md:text-xs font-normal">{data.name}</p>
-          <div className=" text-[8px] md:text-xs mt-5 flex flex-row-reverse justify-between pe-1">
+          <p className="text-[10px] md:text-xs font-normal">{data.name}</p>
+          <div className=" text-[10px] md:text-xs mt-5 flex flex-row-reverse justify-between pe-1">
             <p className="font-bold ">
               {FormatNumberInPersian(data.discountPrice)}
               <span className="font-normal text-[7px] md:text-[10px] ms-1">
                 تومان
               </span>
             </p>
-            <p className="bg-red-500 p-[2px] md:p-1 rounded text-white text-[7px] md:text-xs">
+            <p className="bg-red-500 p-[2px] md:p-1 rounded text-white text-[9px] md:text-xs">
               %
               {Math.round(
                 ((data.price - data.discountPrice) / data.price) * 100
@@ -39,10 +39,10 @@ function CardSpecials({ data }) {
             </p>
           </div>
           <p className="text-gray-500 px-3  mt-2 text-end">
-            <del className="font-normal text-[7px]  md:text-sm ">
+            <del className="font-normal text-[9px]  md:text-sm ">
               {FormatNumberInPersian(data.price)}
             </del>
-            <span className="font-normal text-[6px] md:text-[10px] ms-1">
+            <span className="font-normal text-[8px] md:text-[10px] ms-1">
               تومان
             </span>
           </p>
